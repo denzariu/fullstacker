443d727910b163ebc6a4f2837d0cdcbedb21a272
Denzo <40743579+denzariu@users.noreply.github.com>
2024-03-05 22:24:35 +0200

    - Added Formik for forms.

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   return (
     
     <ApolloProvider client={client}>
-      <Accounts/>
+      {/* <Accounts/> */}
       <LogIn/>
     </ApolloProvider>
   )
--- src/client/components/LogIn.tsx
+++ src/client/components/LogIn.tsx
@@ -1,6 +1,7 @@
 import { ReactElement } from 'react'
 import { GET_ALL_ACCOUNTS } from '../GraphQL/Queries'
 import { useQuery } from '@apollo/client'
+import { Field, Form, Formik } from 'formik'
 
 
 
@@ -17,15 +18,58 @@ function Accounts(): ReactElement {
     <div className="flex h-screen bg-green-50 items-center" 
       // justify-center items-center"
     >
-      <div className='flex-1 flex justify-between mx-8 h-48 border-2 border-lime-500 rounded-lg gap-x-4 p-4'>
-        <div className="text-3xl text-lime-600">Log In</div>
-        <div className='flex flex-col'>
-          <div className="text-xl text-lime-600">Username</div>
-          <input></input>
-          <div className="text-xl text-lime-600">Password</div>
-          <input></input>
-          <button className='self-end mt-2 py-1 px-2 rounded-xl bg-lime-500 text-lime-200'>Submit</button>
-        </div>
+      <div className='flex-1 sm:flex mx-8 lg:mx-96  justify-between border-2 border-lime-200 bg-lime-50 rounded-lg gap-x-4 p-4'>
+        <h1 className="text-3xl text-lime-600 mb-4">Log In</h1>
+
+        
+        
+          <Formik
+            initialValues={{
+              firstName: '',
+              password: '',
+            }}
+            onSubmit={async (values) => {
+              await new Promise((r) => setTimeout(r, 500));
+              alert(JSON.stringify(values, null, 2));
+            }}
+          >
+            <Form className='flex flex-col sm:bg-lime-200 sm:p-8 rounded-lg sm:gap-y-2'>
+              <label 
+                className="text-xl text-lime-600"
+                htmlFor="username"
+              >
+                Username
+              </label>
+              <Field 
+                id="username" 
+                name="username" 
+                placeholder="Jane"
+                className="px-1"
+              >
+              </Field>
+              <label 
+                className="text-xl text-lime-600"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <Field 
+                id="password" 
+                name="password" 
+                placeholder=""
+                type="password"
+                className="px-1"
+              >  
+              </Field>
+              
+              <button 
+                type='submit'
+                className='self-end mt-2 py-1 px-2 rounded-xl bg-lime-500 text-lime-200'
+              >
+                Submit
+              </button>
+            </Form>
+          </Formik>
       </div>
     </div>
 
